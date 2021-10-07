31755efb0faa465ae044e88b510c3e6d0f3a1747
PetePrattis <panagiotispr1997@gmail.com>
2021-10-07 20:59:48 +0300

    implement stateless widget for hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Demo App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[400],
-    ),
-    body: const Center(
-      child: Text(
+void main() => runApp(const MaterialApp(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Demo App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: const Center(
+        child: Text(
           "Flutter App",
           style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey,
-            fontFamily: 'IndieFlowerFamily'
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlowerFamily'
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: const Text('click'),
-      backgroundColor: Colors.red[400],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: const Text('click'),
+        backgroundColor: Colors.red[400],
+      ),
+    );
+  }
+}
